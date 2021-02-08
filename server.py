690490a59b5dcfea01c1564ded5bb17adf2a4c48
Teddy O'Brien <tedster.obrien@gmail.com>
2021-02-07 21:30:56 -0500

    idk

--- server.py
+++ server.py
@@ -5,9 +5,10 @@ import threading
 PORT = 80
 handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print("Serving HTTP at port", PORT)
+with socketserver.TCPServer((, PORT), handler) as httpd:
+    print(Serving HTTP at port, PORT)
     try:
         threading.Thread(target=httpd.serve_forever())
     except KeyboardInterrupt:
         httpd.shutdown()
+
